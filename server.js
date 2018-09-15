c72a8efbe86503761e912a9fb99f37b4975d58e5
pika89 <pikabrelol@gmail.com>
2018-09-15 17:20:52 +0200

    change titles, add description in read me

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(path.join(__dirname, 'dist/untitled2')));
+app.use(express.static(path.join(__dirname, 'dist/phonebook')));
 // Heroku port
 app.listen(process.env.PORT || 8080);
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist//untitled2/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/phonebook/index.html'));
 });
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'untitled2'`, async(() => {
+  it(`should have as title 'phonebook'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('untitled2');
+    expect(app.title).toEqual('phonebook');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to untitled2!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to phonebook!');
   }));
 });
