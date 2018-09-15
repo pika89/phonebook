087b216b33d36eb9b28e338b8d6822eb955d139d
pika89 <pikabrelol@gmail.com>
2018-09-15 16:38:35 +0200

    add server.js

--- server.js
+++ server.js
@@ -3,22 +3,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// Run the app by serving the static files
-// in the dist directory
 app.use(express.static(__dirname + '/dist'));
-const forceSSL = function() {
-  return function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] !== 'https') {
-      return res.redirect(
-        ['https://', req.get('Host'), req.url].join('')
-      );
-    }
-    next();
-  }
-}
-// Start the app by listening on the default
 // Heroku port
-app.use(forceSSL());
 app.listen(process.env.PORT || 8080);
 app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist//untitled2/index.html'));
