c4da2e5ad4be362d6673569d5536d4c161b267c7
pika89 <pikabrelol@gmail.com>
2018-09-15 17:05:12 +0200

    add check for datasource(localstorage data) if null/empty make it = []

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,13 @@ import {Component, OnInit} from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  dataSource: any[];
 
+  ngOnInit(){
+    if(!localStorage.getItem('datasource')){
+      this.dataSource = [];
+      localStorage.setItem('datasource', JSON.stringify(this.dataSource));
+    }
+  }
 }
