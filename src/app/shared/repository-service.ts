10109ff9ae5d789871706e87d81de9c2710700fd
pika89 <pikabrelol@gmail.com>
2019-01-09 01:50:56 +0100

    deleted more stuff

--- src/app/shared/repository-service.ts
+++ src/app/shared/repository-service.ts
@@ -62,9 +62,6 @@ export class RepositoryService {
       // TODO: send the error to remote logging infrastructure
       console.error(error); // log to console instead
 
-      // TODO: better job of transforming error for user consumption
-      this.log(`${operation} failed: ${error.message}`);
-
       // Let the app keep running by returning an empty result.
       return of(result as T);
     };
